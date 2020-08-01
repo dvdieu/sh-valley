6552005052e8b224d1564f8ae642b771a16c9f8a
Shiqiang <cocowool@qq.com>
2020-08-01 23:13:14 +0800

    modify shell

--- shell/file_compare.sh
+++ shell/file_compare.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# 判断 file_compare.sh 这个文件是否存在
 if [[ -e file_compare.sh ]]; then
-	echo "File Exists!"
+	echo "File 'file_compare.sh' Exists!"
 else
-	echo "File doen't exists!"
+	echo "File 'file_compare.sh' doen't exists!"
+fi
+
+# 判断 /etc 目录是否存在
+if [[ -e /etc ]]; then
+	echo "Folder /etc exists!"
+else
+	echo "folder /etc dosn't exists!"
 fi
 
 if [[ -f file_compare ]]; then
@@ -11,3 +19,4 @@ if [[ -f file_compare ]]; then
 else
 	echo "Not common file!"
 fi
+
